176c15fd5520b528d4abc6aabf7a05e2f38b2cc0
Norman Lew <normanlew@users.noreply.github.com>
2026-02-16 13:12:46 -0800

    Darken squares each time the mouse pointer enters it
    Implemented feature whereby each square is darkened every time the mouse
    pointer enters it.

--- javascript.js
+++ javascript.js
@@ -1,38 +1,60 @@
-const grid = document.querySelector("#the_grid");
-// grid.style.display = "flex";
-// grid.style.flexDirection = "column";
+// Create a grid for an etch-a-sketch.  As the mouse pointer is moved across the screen,
+// it leaves a trail!
 
+const GRID_LENGTH_AND_WIDTH = 600;
 
+prompt_button = document.querySelector("#number");
+prompt_button.style.marginBottom = "20px";
 
-function createSquare() {
+createGrid(16);
+
+prompt_button.addEventListener("click", () => {
+    document.querySelector("#the_grid").innerHTML = "";
+    let dimension = prompt("Please enter the number of rows (maximum of 100): ");
+
+    if (dimension > 100) {
+        dimension = 100;
+    }
+
+    createGrid(dimension);
+})
+
+function createGrid(dimension) {
+    let square_length_and_width = GRID_LENGTH_AND_WIDTH/dimension;
+    const grid = document.querySelector("#the_grid");
+
+    for (let j = 0; j < dimension; j++) {  
+        const row = document.createElement("div");
+        row.id = "row_" + j;
+        row.style.display = "flex";
+        for (let i = 0; i < dimension; i++) {
+            const square = createSquare(square_length_and_width);
+            row.appendChild(square);
+        }
+        grid.appendChild(row);
+    }
+}
+
+function createSquare(width_and_height) {
     const one_square = document.createElement("div");
     one_square.classList.add("square");
     one_square.style.backgroundColor = "yellow";
-    one_square.style.width = "50px";
-    one_square.style.height = "50px";
-    one_square.style.borderColor = "black";
-    one_square.style.borderWidth = "1px";
-    // one_square.style.margin = "10px";
-    one_square.style.borderStyle = "solid";
+    one_square.style.width = width_and_height + "px";
+    one_square.style.height = width_and_height + "px";
+    one_square.style.opacity = 1;
 
     one_square.addEventListener("mouseenter", () => {
-        one_square.style.backgroundColor = "pink";
+        if (one_square.style.backgroundColor === "yellow") {
+            one_square.style.backgroundColor = "purple";
+            one_square.style.opacity = .1;
+        }
+        else {
+            if (one_square.style.opacity < 1) {
+                one_square.style.opacity = Number(one_square.style.opacity) + .1;
+            }
+        }
     })
     return one_square;
 }
-
-for (let j = 0; j < 16; j++) {  
-    const row = document.createElement("div");
-    row.id = "row_" + j;
-    row.style.display = "flex";
-    for (let i = 0; i < 16; i++) {
-        const square = createSquare();
-        row.appendChild(square);
-    }
-    grid.appendChild(row);
-}
-
-grid.appendChild(row);
-
 
 
